694ce30efdbb0525e265f3fa12f4185fae8d3fa5
Gabar426 <naman199lal@gmail.com>
2026-04-28 18:32:21 +0530

    conversion of data type

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,28 @@
+let score = "naman"
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1;false =>0
+
+let isLoggedIn = "naman"
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+//1 =>true;
+//"" =>false;
+// "string" =>true
+
+let ANumber = 33
+let StringNumber = String(ANumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
